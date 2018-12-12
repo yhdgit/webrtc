075248575ae11293b5190fde7e5c1250bd79590c
yhd <yihaodong@u51.com>
2018-12-12 18:06:16 +0800

    safg

--- dataChannel1/index.js
+++ dataChannel1/index.js
@@ -1,41 +1,41 @@
 'use strict';
 
-var os = require('os');
 var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
 
 var fileServer = new(nodeStatic.Server)();
-var app = http.createServer(function(req, res) {
+var server = http.createServer(function(req, res) {
   fileServer.serve(req, res);
-}).listen(8080);
+});
+server.listen(8080);
 
-var io = socketIO.listen(app);
+var io = socketIO.listen(server);
 io.sockets.on('connection', function(socket) {
   socket.on('create or join', function(room) {
-    console.log('Received request to create or join room: ' + room);
+    console.log('received request to create or join room: ' + room);
 
     var clientsInRoom = io.sockets.adapter.rooms[room];
     var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
-    console.log('Room: ' + room + ' now has ' + numClients + ' client(s)');
+    console.log('room: ' + room + ' now has ' + numClients + ' client(s)');
 
     if (numClients === 0) {
       socket.join(room);
-      console.log('Client ID: ' + socket.id + ' created room: ' + room);
+      console.log('clientID: ' + socket.id + ' created room: ' + room);
       socket.emit('created', room);
     } else if (numClients === 1) {
-      io.sockets.in(room).emit('other joining', room); // 获取该房间的socket，并通知有其他人加入
+      io.sockets.in(room).emit('other joining', room); // 获取该房间的所有socket，并通知有其他人加入
       socket.join(room);
-      console.log('Client ID: ' + socket.id + ' joined room: ' + room);
-      socket.emit('joined', room); // 通知自己加入了那个房间
+      console.log('clientID: ' + socket.id + ' joined room: ' + room);
+      socket.emit('joined', room);
       io.sockets.in(room).emit('ready');
-    } else { // max two clients
+    } else {
       socket.emit('full', room);
     }
   });
 
   socket.on('message', function(message) {
-    console.log('Client said: ', message);
+    console.log('client said: ', message);
     // for a real app, would be room-only (not broadcast)
     socket.broadcast.emit('message', message);
   });
